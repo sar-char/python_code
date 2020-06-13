058b35498f99f1e09f1d4869c355de7130637367
Sarah Chapman <65090487+sar-char@users.noreply.github.com>
2020-06-13 19:07:29 +0100

    Updated version of code from work laptop

--- export_directory_contents.py
+++ export_directory_contents.py
@@ -1,20 +1,48 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Mon May 18 16:56:57 2020
 
-@author: sarah
-"""
-
+# Owned
+from nostalgia_util import log_utils
+from nostalgia_util import settings_util
+__author__ = 'Sarah Chapman'
+__credits__ = ['Sarah Chapman']
+#__license__ = 'MPL 2.0'
+__version__ = '0.1.0'
+__maintainer__ = 'Sarah Chapman'
+__status__ = 'Production'
 
 #Import Packages
 import os
 import pandas as pd
+import csv
+
+#UserInput
+#inputdir1 = input('Input Directory:')
+#outputdir1 = input('Output Directory:')
 
 #Create Report of OS files
-dir=r'C:\Users\sarah\.spyder-py3'
-list=os.listdir(dir)
-list=pd.DataFrame(list)
-list.to_csv(r'C:\Users\sarah\.spyder-py3\list.csv', index=False)
+inputdir=r'C:\Development\x_reporting_solution\PinPoint.Reports\PinPoint.Reports'
+outputdir=r'C:\Users\UKSMC009'
+outputfile=r'\List15.csv'
 
-#Delete files
-#s.remove(dir+r'\list.csv')
+#Create a blank csv file 
+f=open(outputdir+outputfile, 'w', newline='')  #write to an existing blank csv file
+
+# Headers of csv files
+names = ['FileNameAndPath', 'Name']
+
+#Write to file
+w=csv.writer(f)
+
+#Write header to the file
+w.writerow(names)
+
+#Loop through directory and write root, dir, files to csv file
+for root, dirs, files in os.walk(inputdir):
+    for filename in files:
+         FileNameAndPath=os.path.join(root, filename)
+         Name=filename
+         w.writerow([FileNameAndPath, Name])
+        
+#Close csv
+f.close()
