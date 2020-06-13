7004cb2ca82c54a1e4357f698ab8da9e73e97322
Sarah Chapman <65090487+sar-char@users.noreply.github.com>
2020-06-13 21:24:22 +0100

    Create files_exist_in_directory_output_csv.py

---
+++ files_exist_in_directory_output_csv.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 15:07:36 2020
+
+@author: sarah
+"""
+#Compare files in directory to spreadsheet
+
+#Import Packages
+import os
+import pandas as pd
+import csv
+
+path=r'C:\Users\sarah\.spyder-py3'
+file=r'C:\Users\sarah\.spyder-py3\list.csv'
+new=r'C:\Users\sarah\.spyder-py3\output.csv'
+
+#Read in CSV File
+list=pd.read_csv(file, header=None,skiprows=[0], dtype=str, names=['File'],usecols=[0], squeeze=True)
+print(list)
+
+# Create a workbook and add a worksheet.
+with open(new, 'w', newline='') as f:
+    writer = csv.writer(f)
+    
+#Check if each file exists or not
+for files in list:
+   dir=os.path.join(path,files)
+   if os.path.exists(dir):
+       #print(dir,'exists')
+       exists=dir+'exists'
+       #writer.writerow(files)
+   else:
+       #print(dir,'not exists')
+       notexists=dir+'not exists'
+       writer.writerow([exists, notexists])
+       
+#Ouput results to csv
+#file.close()
